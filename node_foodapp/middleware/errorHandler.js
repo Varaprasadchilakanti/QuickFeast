871d3083da9c6bffe68662e9d97c7aa263abebfb
Vara Prasad <varaprasadchilakanti@gmail.com>
2024-12-18 07:15:18 +0530

    Added backend submodules and moved existing files

--- node_foodapp/middleware/errorHandler.js
+++
@@ -1,6 +0,0 @@
-const { logger } = require('../config/logger');
-
-module.exports = (err, req, res, next) => {
-    logger.error(err.stack);
-    res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
-};
--- node_foodapp/models/Query.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const QuerySchema = new mongoose.Schema({
-    id: { type: String, required: true },
-    query: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Query', QuerySchema);
